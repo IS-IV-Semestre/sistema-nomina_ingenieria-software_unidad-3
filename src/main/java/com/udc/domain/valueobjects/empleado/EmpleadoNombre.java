f3cd927bfa5b38897721dea86707e3b540e2321a
kevin-aspire <kgomezr2@unicartagena.edu.co>
2026-05-21 16:40:14 -0500

    feat: implementacion de excepciones en EmpleadoNombre

--- src/main/java/com/udc/domain/valueobjects/empleado/EmpleadoNombre.java
+++ src/main/java/com/udc/domain/valueobjects/empleado/EmpleadoNombre.java
@@ -1,11 +1,26 @@
 package com.udc.domain.valueobjects.empleado;
 
+import com.udc.domain.exceptions.empleado.InvalidEmpleadoNombre;
+
 public record EmpleadoNombre(String value) {
     public EmpleadoNombre(String value) {
         String normilizedValue = value.trim();
 
+        // Comprobar que no este vacio
         if(normilizedValue.isBlank() || normilizedValue.isBlank()){
-            throw new IllegalArgumentException("El nombre del empleado no puede estar vacio");
+            throw InvalidEmpleadoNombre.becauseIsEmpty();
+        }
+
+        if(normilizedValue.length() > 50){
+            throw InvalidEmpleadoNombre.becauseIsTooLong();
+        }
+
+        if(normilizedValue.length() < 3){
+            throw InvalidEmpleadoNombre.becauseIsTooShort();
+        }
+
+        if(!normilizedValue.matches("[A-Za-z]+")){
+            throw InvalidEmpleadoNombre.becauseHaveInvalidFormat();
         }
 
         this.value = value;
