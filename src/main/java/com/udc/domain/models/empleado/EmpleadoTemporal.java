6a2073135bee587ad6b602c1f1e21e10dd1a7b75
ngamaj-est <nganaj@unicartagena.edu.co>
2026-05-21 20:34:43 -0500

    feat: construccion de la clase empleado-temporal

---
+++ src/main/java/com/udc/domain/models/empleado/EmpleadoTemporal.java
@@ -0,0 +1,36 @@
+package com.udc.domain.models.empleado;
+
+import com.udc.domain.enums.empleado.tipoDocumento;
+
+public class EmpleadoTemporal extends Empleado {
+    protected int duracionContrato; 
+    protected double salarioMensualFijo;
+
+    public EmpleadoTemporal(String nombre, String apellido, int duracionContrato, double salarioMensualFijo, tipoDocumento tipoDocumento, 
+        String documento) {
+            super(nombre, apellido, duracionContrato, tipoDocumento, documento);
+            if (duracionContrato <= 0) {
+                throw new IllegalArgumentException("La duración del contrato debe ser mayor a cero.");
+            }
+            if (salarioMensualFijo <= 0) {
+                throw new IllegalArgumentException("El salario mensual debe ser mayor a cero.");
+            }
+            this.duracionContrato = duracionContrato;
+            this.salarioMensualFijo = salarioMensualFijo;
+            
+    }
+    @Override
+    public double calcularBeneficios() {
+        return 0;
+    } 
+    @Override
+    public double calcularSalarioBruto() {
+        return salarioMensualFijo;
+    }
+    public double getDuracionContrato() {
+        return duracionContrato;
+    }
+    public double getSalarioMensualFijo() {
+        return salarioMensualFijo;
+    }
+}
