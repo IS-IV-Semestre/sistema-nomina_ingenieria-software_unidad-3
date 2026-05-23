73bb99902964997e28bf741c7ab1eb1f636fb107
Sergio Vargas <sergiolvargas95@gmail.com>
2026-05-23 09:11:54 -0500

    DN-30: Definir puerto de salida IEmpeladoRepository

---
+++ src/main/java/com/udc/application/port/out/UpdateEmpleadoPorComisionPort.java
@@ -0,0 +1,7 @@
+package com.udc.application.port.out;
+
+import com.udc.domain.models.empleado.EmpleadoPorComision;
+
+public interface UpdateEmpleadoPorComisionPort {
+    EmpleadoPorComision update(EmpleadoPorComision empleadoPorComision);
+}
